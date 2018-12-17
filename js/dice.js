f7cc53bd0a8eec13030867d1125b2e96b7b3d850
Njarif <fmwangi38@gmail.com>
2018-12-17 10:31:41 +0300

    javascript & html for user inputs and interger outputs

---
+++ js/dice.js
@@ -0,0 +1,47 @@
+var playerOne = ""
+var playerTwo = ""
+
+function PlayGame() {
+  this.dicerolled = 0;
+  this.currentresult = 0;
+  this.totalscore = 0;
+}
+
+PlayGame.prototype.diceRoll = function() {
+  if (this.dicerolled === 1) {
+    this.currentresult = 0;
+    alert("You rolled a 1");
+  } else {
+    this.currentresult += this.dicerolled
+  }
+}
+PlayGame.prototype.total = function() {
+  this.totalscore += this.currentresult
+}
+
+var roll = function() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+$(document).ready(function() {
+  playerOne = new PlayGame(true);
+  playerTwo = new PlayGame(true);
+
+  $("#roller1").click(function() {
+    playerOne.dicerolled = roll();
+    $("#roll1").val(playerOne.dicerolled)
+    playerOne.diceRoll();
+    $("#result1").val(playerOne.currentresult);
+    playerOne.total();
+    $("#score1").val(playerOne.totalscore);
+  });
+});
+$(document).ready(function() {
+  $("#roller2").click(function() {
+    playerTwo.dicerolled = roll();
+    $("#roll2").val(playerTwo.dicerolled)
+    playerTwo.diceRoll();
+    $("#result2").val(playerTwo.currentresult);
+    playerTwo.total();
+    $("#score2").val(playerTwo.totalscore);
+  });
+});
