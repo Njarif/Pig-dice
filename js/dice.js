c281940e3ea421eee5ee7f7204d5ee7549b28faa
Njarif <fmwangi38@gmail.com>
2018-12-18 08:26:07 +0300

    changes to javascript and css

--- js/dice.js
+++ js/dice.js
@@ -18,30 +18,34 @@ PlayGame.prototype.diceRoll = function() {
 PlayGame.prototype.total = function() {
   this.totalscore += this.currentresult
 }
+PlayGame.prototype.hold = function() {
+  alert("Your turn is over");
+  this.totalscore += this.currentresult
+}
 
 var roll = function() {
-  return Math.floor(Math.random() * 6) + 1;// returns a random integer from 1 to 6
+  return Math.floor(Math.random() * 6) + 1; // returns a random integer from 1 to 6
 }
 $(document).ready(function() {
-  playerOne = new PlayGame(true);
-  playerTwo = new PlayGame(true);
+      playerOne = new PlayGame(true);
+      playerTwo = new PlayGame(true);
 
-  $("#roller1").click(function() {
-    playerOne.dicerolled = roll();
-    $("#roll1").val(playerOne.dicerolled)
-    playerOne.diceRoll();
-    $("#result1").val(playerOne.currentresult);
-    playerOne.total();
-    $("#score1").val(playerOne.totalscore);
-  });
-});
-$(document).ready(function() {
-  $("#roller2").click(function() {
-    playerTwo.dicerolled = roll();
-    $("#roll2").val(playerTwo.dicerolled)
-    playerTwo.diceRoll();
-    $("#result2").val(playerTwo.currentresult);
-    playerTwo.total();
-    $("#score2").val(playerTwo.totalscore);
-  });
-});
+      $("#roller1").click(function() {
+        playerOne.dicerolled = roll();
+        $("#roll1").val(playerOne.dicerolled)
+        playerOne.diceRoll();
+        $("#result1").val(playerOne.currentresult);
+        playerOne.total();
+        $("#score1").val(playerOne.totalscore);
+        });
+      });
+      $(document).ready(function() {
+        $("#roller2").click(function() {
+          playerTwo.dicerolled = roll();
+          $("#roll2").val(playerTwo.dicerolled)
+          playerTwo.diceRoll();
+          $("#result2").val(playerTwo.currentresult);
+          playerTwo.total();
+          $("#score2").val(playerTwo.totalscore);
+        });
+      });
